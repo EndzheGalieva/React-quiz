93e22f8cb0e512fe51d993a8659af3ee770ae2c8
Endzhe Galieva <endzhe91@gmail.com>
2020-10-09 21:42:59 +0300

    changed  page authorization

--- src/components/Navigation/Drawer/Drawer.js
+++ src/components/Navigation/Drawer/Drawer.js
@@ -3,14 +3,6 @@ import classes from './Drawer.css'
 import {NavLink} from 'react-router-dom'
 import Backdrop from '../../UI/Backdrop/Backdrop'
 
-const links = [
-  {to: '/', label: 'Список', exact: true},
-  {to: '/auth', label: 'Авторизация', exact: false},
-  {to: '/quiz-creator', label: 'Создать тест', exact: false}
-
-
-]
-
 class Drawer extends Component {
 
   clickHandler = () => {
@@ -18,7 +10,7 @@ class Drawer extends Component {
   }
 
 
-  renderLinks() {
+  renderLinks(links) {
     return links.map((link, index) => {
       return (
         <li key={index}>
@@ -41,11 +33,24 @@ class Drawer extends Component {
        cls.push(classes.close) 
     }
 
+    const links = [
+      {to: '/', label: 'Список', exact: true}
+    ]
+
+    console.log('Auth', this.props.isAuthenticated)
+
+    if (this.props.isAuthenticated) {
+      links.push({to: '/quiz-creator', label: 'Создать тест', exact: false})
+      links.push({to: '/logout', label: 'Выйти', exact: false})
+    } else {
+      links.push({to: '/auth', label: 'Авторизация', exact: false})
+    }
+
     return (
       <React.Fragment>
        <nav className={cls.join(' ')}>
         <ul>
-         { this.renderLinks() }
+         { this.renderLinks(links) }
         </ul>
         </nav>
         { this.props.isOpen ? <Backdrop onClick={this.props.onClose} /> : null }
--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -3,9 +3,10 @@ import classes from './Auth.css'
 import Button from '../../components/UI/Button/Button'
 import Input from '../../components/UI/input/input'
 import is from 'is_js'
-import axios from 'axios'
+import {connect} from 'react-redux'
+import {auth} from '../../store/actions/auth'
 
-export default class Auth extends Component {
+class Auth extends Component {
 
 
   state = {
@@ -38,34 +39,21 @@ export default class Auth extends Component {
     }
   }
 
-  loginHandler = async () => {
-    const authData = {
-      email: this.state.formControls.email.value,
-      password: this.state.formControls.password.value,
-      returnSecureToken: true
-    }
-  try {
-    const response = await axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBn0U73CJn7RgF8wOZ_JEiKKKyMrqgCMxU', authData)
+  loginHandler = () => {
+    this.props.auth(
+      this.state.formControls.email.value,
+      this.state.formControls.password.value,
+      true
+    )
+ }
 
-    console.log(response.data)
-  } catch (e) {
-    console.log(e)
-  }
-  }
-
-  registerHandler = async () => {
-    const authData = {
-      email: this.state.formControls.email.value,
-      password: this.state.formControls.password.value,
-      returnSecureToken: true
-    }
-  try {
-    const response = await axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBn0U73CJn7RgF8wOZ_JEiKKKyMrqgCMxU', authData)
-
-    console.log(response.data)
-  } catch (e) {
-    console.log(e)
-  }
+  registerHandler = () => {
+    this.props.auth(
+      this.state.formControls.email.value,
+      this.state.formControls.password.value,
+      false
+    )
+    
 }
 
   submitHandler = event => {
@@ -166,3 +154,11 @@ export default class Auth extends Component {
      )
    }
 }
+
+function mapDispatchToProps(dispatch) {
+  return {
+    auth: (email, password, isLogin) => dispatch(auth(email, password, isLogin))
+  }
+}
+
+export default connect(null, mapDispatchToProps)(Auth)
